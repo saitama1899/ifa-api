944f27f2df2beea4efc300e92124a200bd8b39e7
saitama1899 <eric.lorien92@gmail.com>
2023-05-22 11:56:36 +0200

    first endpoint

---
+++ server.js
@@ -0,0 +1,35 @@
+app.post('/api/infojobs', async (req, res) => {
+    const accessToken = req.body.accessToken;
+    const offerId = req.body.offerId;
+
+    try {
+        // First, get the list of curriculums
+        const curriculumListResponse = await axios.get('https://api.infojobs.net/api/2/curriculum', {
+            headers: { 'Authorization': `Bearer ${accessToken}` },
+        });
+
+        // Extract the curriculumId from the first curriculum in the list
+        // (or implement your own logic for choosing which curriculum to use)
+        const curriculumId = curriculumListResponse.data[0].id;
+
+        const endpoints = [
+            `/api/1/curriculum/${curriculumId}/cvtext`,
+            `/api/1/curriculum/${curriculumId}/education`,
+            `/api/1/curriculum/${curriculumId}/experience`,
+            `/api/1/curriculum/${curriculumId}/futurejob`,
+            `/api/1/curriculum/${curriculumId}/skill`,
+            `/api/1/offer/${offerId}`,
+        ];
+
+        const responses = await Promise.all(endpoints.map(endpoint =>
+            axios.get(`https://api.infojobs.net${endpoint}`, {
+                headers: { 'Authorization': `Bearer ${accessToken}` },
+            })
+        ));
+
+        const data = responses.map(response => response.data);
+        res.json(data);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
