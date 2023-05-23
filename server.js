e72d105fd85397cddaf2daedd932de0c7849e763
saitama1899 <eric.lorien92@gmail.com>
2023-05-23 14:01:55 +0200

    delete debug

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.post('/api/infojobs', async (req, res) => {
         const data = responses.map(response => response.data);
         res.json(data);
     } catch (error) {
-        res.status(500).send(error.message, accessToken);
+        res.status(500).send(error.message)
     }
 });
 
