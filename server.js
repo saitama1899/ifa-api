eb09d99ed5709f6070bfcf8f30ba49a1919c1943
saitama1899 <eric.lorien92@gmail.com>
2023-05-23 15:52:20 +0200

    add debug

--- server.js
+++ server.js
@@ -28,28 +28,28 @@ app.post('/api/infojobs', async (req, res) => {
 
         const curriculumPrincipal = curriculumListResponse.data.find(curriculum => curriculum.principal)
         const curriculumId = curriculumPrincipal.code
-        // res.json(curriculumId)
+        res.json(curriculumId)
 
-        const endpoints = [
-            `/api/1/curriculum/${curriculumId}/cvtext`,
-            `/api/1/curriculum/${curriculumId}/education`,
-            `/api/1/curriculum/${curriculumId}/experience`,
-            `/api/1/curriculum/${curriculumId}/futurejob`,
-            `/api/1/curriculum/${curriculumId}/skill`,
-            // `/api/1/offer/${offerId}`,
-        ];
+        // const endpoints = [
+        //     `/api/1/curriculum/${curriculumId}/cvtext`,
+        //     `/api/1/curriculum/${curriculumId}/education`,
+        //     `/api/1/curriculum/${curriculumId}/experience`,
+        //     `/api/1/curriculum/${curriculumId}/futurejob`,
+        //     `/api/1/curriculum/${curriculumId}/skill`,
+        //     // `/api/1/offer/${offerId}`,
+        // ];
 
-        const responses = await Promise.all(endpoints.map(endpoint =>
-            axios.get(`https://api.infojobs.net${endpoint}`, {
-                headers: { 
-                    'Authorization': `Basic ${hash}, Bearer ${accessToken}`,
-                    'Content-Type': 'application/json',
-                },            
-            })
-        ));
+        // const responses = await Promise.all(endpoints.map(endpoint =>
+        //     axios.get(`https://api.infojobs.net${endpoint}`, {
+        //         headers: { 
+        //             'Authorization': `Basic ${hash}, Bearer ${accessToken}`,
+        //             'Content-Type': 'application/json',
+        //         },            
+        //     })
+        // ));
 
-        const data = responses.map(response => response.data);
-        res.json(data);
+        // const data = responses.map(response => response.data);
+        // res.json(data);
     } catch (error) {
         res.status(500).send(error.message)
     }
